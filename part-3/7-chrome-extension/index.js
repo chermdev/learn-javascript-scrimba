1a695ad2f671ec299abb2c7815b25bcf03f002d4
ChermDev <carlos_e8246@hotmail.com>
2023-01-09 01:53:29 -0600

    part 3 - chrome extension finished

--- part-3/7-chrome-extension/index.js
+++ part-3/7-chrome-extension/index.js
@@ -1,23 +1,47 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
+
+let localLeads = JSON.parse(localStorage.getItem("myLeads"))
+
+if (localLeads) {
+    myLeads = localLeads
+    render(myLeads)
+}
 
 inputBtn.addEventListener("click", function () {
     if (inputEl.value) {
         myLeads.push(inputEl.value)
-        renderLeads()
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
         inputEl.value = ""
+        render(myLeads)
     }
 })
 
-function renderLeads() {
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.removeItem("myLeads")
+    myLeads = []
+    render(myLeads)
+})
+
+function render(leads) {
     // let listItems = document.createDocumentFragment()
-    // for (let i = 0; i < myLeads.length; i++) {
+    // for (let i = 0; i < leads.length; i++) {
     //     const li = document.createElement('li')
     //     const a = document.createElement('a')
-    //     a.textContent = myLeads[i]
-    //     a.href = myLeads[i]
+    //     a.textContent = leads[i]
+    //     a.href = leads[i]
     //     a.target = "_blank"
     //     li.append(a)
     //     listItems.append(li)
@@ -25,14 +49,14 @@ function renderLeads() {
     // ulEl.replaceChildren(listItems)
 
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
+            </a>
             </li>
-        `
+            `
     }
     ulEl.innerHTML = listItems
 }
